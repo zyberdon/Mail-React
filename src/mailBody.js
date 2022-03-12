97ad5be9584e78061b7f3eb20195385ad6a238db
zyberdon <srhl383@gmail.com>
2022-03-12 15:25:41 +0530

    few functionalities

--- src/mailBody.js
+++ src/mailBody.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import './App.css';
 import { Row, Col } from 'reactstrap';
+import {folders } from  '../inbox'
 class MailBody extends React.Component {
     constructor(props){
         super(props);
-        this.state={}
+        this.state={
+          mailbody:''
+        };
+        
     }
+
+    view=(e,ele)=>{
+      e.preventDefault()
+      this.props.markAsRead(ele)
+this.setState({mailbody: ele.content})
+    }
+    delete=(e,ele)=>{
+      e.preventDefault()
+      this.props.delete(ele)
+    }
+    flag=(e,ele)=>{
+      e.preventDefault()
+      this.props.flag(ele)
+    }
+    
+componentDidUpdate=(prevProps, prevState) =>{
+  if(prevProps.activeFolder!==this.props.activeFolder)
+  this.setState({mailbody: ''})
+ } 
   render() {
     return (
       // pass the store into the provider
       
-        <div>
-           <Row className='navbar'><Col md={1} >Outlook</Col><Col md={11}></Col></Row>
-          <Row>
-              <Col md={2} className='sidebar'>
-          <p className='sidebarcontent'>inbox</p>
-          <p className='sidebarcontent'>outbox</p>
-          </Col>
-          
-          <Col md={10}>
-          <MailBody {...this.props} />
-          </Col>
-          
-          </Row>
+        <div className='section'>
+           <h1 className='content'>
+           {this.props.content.map(ele=>{
+              return <div key={ele.subject}><span onClick={e=>this.view(e, ele)}>{ele.subject}</span>
+              <span style={{fontSize: '14px'}}> {(ele.flag? '...flagged!' : '')}</span>
+              <span style={{float: 'right'}}>
+              {this.props.activeFolder!=='deleted'&&<button key={ele.id} onClick={(e)=>this.delete(e,ele)}>delete {ele.subject}</button>}
+              {!ele.flag&&<button onClick={(e)=>this.flag(e,ele)}>flag! {ele.subject}</button>}
+              </span>
+              </div>
+           })}
+           
+           </h1>
+           <p className='mailbody'>
+{this.state.mailbody}</p>
         </div>
     )
   }
--- src/outlook.js
+++ src/outlook.js
@@ -4,34 +4,82 @@ import NavbarOutlook from './navbar'
 import Sidebar from "react-sidebar";
 import { Row, Col } from 'reactstrap';
 import MailBody from './mailBody';
+import {folders } from  '../inbox'
 
 class Outlook extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            folders: ['inbox','sent','junk email','drafts','deleted']
+          activeFolder: 'inbox',
+            folders: ['inbox','sent','junk','drafts','deleted'],
+            mail: folders
         }
     }
+
+    changefolders=(e, folder)=>{
+      this.setState({activeFolder : folder})
+    }
+    calcUnread=(element)=>{
+      let unread =0
+      if(this.state.mail[element])
+      this.state.mail[element].forEach(ele=>{
+        ele.unread && unread++
+      })
+      return unread
+    }
+    markAsRead=(ele)=>{
+      let newEmails= this.state.mail;
+      let id = ele.mId
+      newEmails[this.state.activeFolder].map(e=>{
+        if(e.mId===id)
+          e.unread = false
+        return e
+      })
+      console.log(newEmails)
+      this.setState({mail: newEmails})
+    }
+
+    delete=(ele)=>{
+      let newEmails= this.state.mail;
+      let x= [];
+       newEmails[this.state.activeFolder].forEach(e=>{
+        if(e.mId!==ele.mId)
+            x.push(e)
+        else 
+            newEmails.deleted.push(e)
+      })
+
+      newEmails[this.state.activeFolder] = x
+      this.setState({mail: newEmails})
+    }
+flag=(ele)=>{
+  let newEmails= this.state.mail;
+  let x= [];
+   newEmails[this.state.activeFolder].map(e=>{
+     if(e.mId===ele.mId)
+    e.flag=true
+  })
+  this.setState({mail: newEmails})
+}
   render() {
     return (
       // pass the store into the provider
       
         <div>
            <Row className='navbar'><Col md={1} >Outlook</Col><Col md={11}></Col></Row>
-          <Row>
-              <Col md={2} className='sidebar'>
+              <Row className='container'><Col md={2} className='sidebar' style={{'width': '10%'}}>
               {
-    this.state.folders.map(ele=>{
-        return<p key={ele} className='sidebarcontent' onClick={(event)=> this.changefolders(event,ele)}>{ele}</p>
+    Object.keys(this.state.mail).map(ele=>{
+      let unread=this.calcUnread(ele)
+      
+        return<p key={ele} className='sidebarcontent' onClick={(event)=> this.changefolders(event,ele)}>{ele + unread}</p>
     })
               }
           </Col>
           
           <Col md={10}>
-          <MailBody {...this.props} />
-          </Col>
-           
-          </Row>
+          <MailBody {...this.props} content ={this.state.mail[this.state.activeFolder]} markAsRead={this.markAsRead} activeFolder={this.state.activeFolder} delete={this.delete} flag={this.flag}/>
+          </Col></Row>
         </div>
     )
   }
